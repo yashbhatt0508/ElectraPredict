f703a591ad3592a685a7fb25e1c8b60467756ca4
yash <uchihayash001@gmail.com>
2026-04-28 13:56:48 +0530

    Update API URL to production backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -16,4 +16,3 @@ export const predictDemand = async (features) => {
     throw error;
   }
 };
-
